d7f67a0ca4fb6d079c744679125be4e18123e69f
yohan <yohanbinan22@gmail.com>
2024-03-26 23:44:06 +0100

    Implemented DQN network

--- Classics/agent/algo.py
+++ Classics/agent/algo.py
@@ -1,5 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
 import random
+from collections import deque
 
 class AgentAbstract:
     def __init__(self, action_space_size):
@@ -102,3 +106,80 @@ class SARSAAgent(AgentAbstract):
         This method would need specific implementation based on the game's state representation.
         """
         pass
+
+
+class DQN(nn.Module):
+    def __init__(self, state_size, action_size):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(state_size, 24)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(24, 24)
+        self.fc3 = nn.Linear(24, action_size)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        return self.fc3(x)
+
+
+class DQNAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=2000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.995
+        self.learning_rate = 0.001
+        self.model = DQN(state_size, action_size)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        state = torch.FloatTensor(state).unsqueeze(0)  # Convert state to tensor and add batch dimension
+        act_values = self.model(state)
+        return np.argmax(act_values.cpu().data.numpy())
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                next_state = torch.FloatTensor(next_state).unsqueeze(0)
+                target = (reward + self.gamma *
+                          np.amax(self.model(next_state).cpu().data.numpy()))
+            state = torch.FloatTensor(state).unsqueeze(0)
+            target_f = self.model(state).cpu().data.numpy()
+            target_f[0][action] = target
+            target_f = torch.FloatTensor(target_f)
+            self.optimizer.zero_grad()
+            output = self.model(state)
+            loss = nn.MSELoss()(output, target_f)
+            loss.backward()
+            self.optimizer.step()
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+
+# agent = DQNAgent(state_size=env.observation_space.shape[0], action_size=env.action_space.n)
+# episodes = 1000
+
+# for e in range(episodes):
+#     state = env.reset()
+#     state = np.reshape(state, [1, state_size])
+#     for time in range(500):
+#         action = agent.act(state)
+#         next_state, reward, done, _ = env.step(action)
+#         next_state = np.reshape(next_state, [1, state_size])
+#         agent.remember(state, action, reward, next_state, done)
+#         state = next_state
+#         if done:
+#             print("episode: {}/{}, score: {}".format(e, episodes, time))
+#             break
+#     if len(agent.memory) > 32:
+#         agent.replay(32)
