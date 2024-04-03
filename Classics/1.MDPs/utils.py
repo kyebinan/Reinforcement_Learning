473f1dceecb01725fe23bad163d10cb9c3f50cc9
yohan <yohanbinan22@gmail.com>
2024-04-03 21:52:05 +0200

    Creating the files for the MDPs lab

---
+++ Classics/1.MDPs/utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import random
+from IPython import display
+
+
+# Implemented methods
+methods = ['DynProg', 'ValIter'];
+
+# Some colours
+LIGHT_RED     = '#FFC4CC';
+LIGHT_GREEN   = '#95FD99';
+BLACK         = '#000000';
+WHITE         = '#FFFFFF';
+GREY          = '#D1CDDC';
+LIGHT_PURPLE  = '#E8D0FF';
+LIGHT_ORANGE  = '#FAE0C3';
+BRIGHT_PURPLE = '#8000FF';
+BRIGHT_ORANGE = '#F57A09';
